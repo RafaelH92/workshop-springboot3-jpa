3139dc2e6f0e5734b91977ccc335035e123b5510
Rafael H. Souza <rafaelsrt8@gmail.com>
2025-04-21 11:40:40 -0300

    Adicionado camada de serviço e registro de componente

--- src/main/java/com/javaweb/course/resources/UserResource.java
+++ src/main/java/com/javaweb/course/resources/UserResource.java
@@ -1,19 +1,32 @@
 package com.javaweb.course.resources;
 
 import com.javaweb.course.entities.User;
+import com.javaweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController /* Recurso web que é implementado por um controlador Rest */
 @RequestMapping(value = "/users") /* Nome do recurso */
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping /* Indica que esse metodo que responde a requisicao do tipo get */
-    public ResponseEntity<User> findAll(){
-        /* Apenas para teste */
-        User usuario = new User(1L, "Rafa", "rafateste@email.com", "9999999", "senha");
-        return ResponseEntity.ok().body(usuario);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}") /* Indica que o endpoint tera um parametro */
+    public ResponseEntity<User> findById(@PathVariable Long id){ /* Passa o parametro definido no endpoint para metodo */
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
