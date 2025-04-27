654fb5da41297064ddf9ba45b0795cde9b92f7e2
Rafael H. Souza <rafaelsrt8@gmail.com>
2025-04-26 21:57:45 -0300

    Adicionado entidade categoria

---
+++ src/main/java/com/javaweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.javaweb.course.repositories;
+
+import com.javaweb.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
--- src/main/java/com/javaweb/course/resources/CategoryResource.java
+++ src/main/java/com/javaweb/course/resources/CategoryResource.java
@@ -1,7 +1,7 @@
 package com.javaweb.course.resources;
 
-import com.javaweb.course.entities.Order;
-import com.javaweb.course.services.OrderService;
+import com.javaweb.course.entities.Category;
+import com.javaweb.course.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController /* Recurso web que é implementado por um controlador Rest */
-@RequestMapping(value = "/orders") /* Nome do recurso */
-public class OrderResource {
+@RequestMapping(value = "/categories") /* Nome do recurso */
+public class CategoryResource {
 
     @Autowired
-    private OrderService service;
+    private CategoryService service;
 
     @GetMapping /* Indica que esse metodo que responde a requisicao do tipo get */
-    public ResponseEntity<List<Order>> findAll(){
-        List<Order> list = service.findAll();
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}") /* Indica que o endpoint tera um parametro */
-    public ResponseEntity<Order> findById(@PathVariable Long id){ /* Passa o parametro definido no endpoint para metodo */
-        Order order = service.findById(id);
-        return ResponseEntity.ok().body(order);
+    public ResponseEntity<Category> findById(@PathVariable Long id){ /* Passa o parametro definido no endpoint para metodo */
+        Category category = service.findById(id);
+        return ResponseEntity.ok().body(category);
     }
 }
