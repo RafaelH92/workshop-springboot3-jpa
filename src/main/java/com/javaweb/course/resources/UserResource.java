2f06cc42c7b27b8c30b05df43d6b0ea325f5d3c6
Rafael H. Souza <rafaelsrt8@gmail.com>
2025-05-04 16:42:23 -0300

    Adicionado metodo/endpoint para ciação de usuario

--- src/main/java/com/javaweb/course/resources/UserResource.java
+++ src/main/java/com/javaweb/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.javaweb.course.entities.User;
 import com.javaweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController /* Recurso web que é implementado por um controlador Rest */
@@ -28,5 +27,13 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){ /* Passa o parametro definido no endpoint para metodo */
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){ /* Desserializa o json passado para objeto */
+        obj = service.insert(obj);
+        /* Para retorno de reposta 201 (criação) deve ser retornar um obj do tipo URI com o caminho do recurso que foi criado */
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
